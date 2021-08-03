6705f17b6e3cf37c3514395838be35a41ec1a302
mnicklass <m.nicklas.schmidt@gmail.com>
2021-08-02 20:30:28 -0400

    new database + routes

--- app.js
+++ app.js
@@ -13,9 +13,14 @@ app.use(express.json());
 //     key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
 //     cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
 // }, app) 
+const password = 'n060895sdd180994r' // Password for 'Alarm-Server-Authentication' which is passed as an extra header with the requests sent to this server
 
-const webhooks = new Datastore('webhooks.db');
+const webhooks = new Datastore('webhooks.db'); // database where the webhooks potentially get stored, maybe unneccessary 
 webhooks.loadDatabase();
+const alarms = new Datastore('alarms.db');
+alarms.loadDatabase();
+
+// create new Database for alarms, that will cross reference with webhooks.db 
 
 app.get('/', (req, res) =>{
     res.send('Hello World');
@@ -26,11 +31,15 @@ app.get('/api/webhooks', (req, res) => {
         if(err){
             return res.status(404).send('The database was not found!');
         }else{
-            if(docs.length === 0){
-                return res.send('The database is empty!');
+            if(req.header('Alarm-Server-Authentication')!== password){
+                res.status(401).send('You are not authorized to make this request!')
             }else{
-            return res.send(docs);
-            }
+                if(docs.length === 0){
+                    return res.send('The database is empty!');
+                }else{
+                return res.send(docs);
+                }
+            }        
         }
     });
 });
@@ -41,18 +50,30 @@ app.get('/api/webhooks/:id', (req, res) => {
           console.log('An error has occurred, ', err);
           return res.status(404).send('The webhook ID was not found!')
        }else{
+        if(req.header('Alarm-Server-Authentication')!== password){
+            res.status(401).send('You are not authorized to make this request!')
+        }else{
            return res.send(docs);
+        }
        }
    })
 });
 
-app.post('/api/webhooks', (req, res) =>{
+app.post('/api/webhooks', (req, res) =>{ 
+    if(req.header('Alarm-Server-Authentication')!== password){
+        res.status(401).send('You are not authorized to make this request!')
+    }else{
     const webhook = req.body; 
     webhooks.insert(webhook);
     res.send(webhook);
+    // Insert here: check if shop && product_id in alarm, if yes check if webhook inventory <= alarm inventory, if yes send nodemail
+    }
 });
 
 app.delete('/api/webhooks/:id', (req, res) => {
+    if(req.header('Alarm-Server-Authentication')!== password){
+        res.status(401).send('You are not authorized to make this request!')
+    }else{
     webhooks.remove({id: req.body.id},{}, (err, numRemoved) => {
         if(err){
            console.log('An error has occurred, ', err);
@@ -61,7 +82,21 @@ app.delete('/api/webhooks/:id', (req, res) => {
             return res.send(`${numRemoved} Webhook with ID: ${req.body.id} has been deleted`);
         }
     })
+    }
+    });
+
+
+app.post('/api/alarms', (req, res) =>{
+    if(req.header('Alarm-Server-Authentication')!== password){
+        res.status(401).send('You are not authorized to make this request!')
+    }else{
+    const alarm = req.body; 
+    alarms.insert(alarm);
+    res.send(alarm);
+    }
 });
+
+// put, get?, delete request for api/alarms
 
 const port = process.env.PORT || 3000;
 
